ec080947af846d175e26e766d27a364ecf133dd8
Dean <deanpatrickgaffney@gmail.com>
2018-03-07 14:10:53 +0000

    added jwt token to agent for iris authentication

--- src/com/wit/iris/main/Main.java
+++ src/com/wit/iris/main/Main.java
@@ -23,9 +23,10 @@ import com.wit.iris.functional.webdrivers.DriverFactory;
 
 
 public class Main {
+	private static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOiJINHNJQUFBQUFBQUFBSlZTdjBcL2JRQlIrRG9rb1FpcFFpVW9NZENuZGtDUEJtS21ndEJLeVFrV2FoVXF0THZiREhKenZ6TjBaa2dWbGdpRURxQzFTSlZaR1wvaE5ZK2dkVVpXQmw3dHAzaHVEQWdyakpmdmY1K1wvVjhmZ01WbzJFeDFvd0w0NmNpaTduMFRhcTVqQTJHbWVhMjYyY0dkWVEyUjN6TWdTMmF3TzN4U3VBRlVPS1JoVmZCRnR0bFZjRmtYRjF0YjJGb2F4ME5DMHJIZDR3Ym1pVzRwXC9TMmY4OGRLbzBQQkFwcTc2d0VvK3N3eGNKUVpkSTJsS3gzVXE0eFdvZkpZaGFvY051TnBrTzZRV2s1RTJZWU9vcVN0UVZHQVl5enpHNHFVdVZvTEV6Y21zMHNGOVVtMmxvQUwxSm1ETGw3bEtScG5YVjM3MnhLU3JBRCsxRHVwQjRkNnU2ZGdcL3FPeDE5V1FsQnFycVNaYThsRVJYeURPM0hpNzgxK1wvMzEwMm11VkFLaVQrYWVcL0tlWXpTOUM3K1BydlRWNjBGMXA0UFdTOWdOVTZLYm1aS3BnXC9hM1RLZjM1OStuRnljXC9obGhKUWQ0c1B6OXpIM1wvcTY1N3JKS1VxYVpWVU03SXRxOXNuc204cVdueVFkYjZQcE5ucVFDNlkrU0ZxTjdpWUtZNHBhMUVvTytMWXl0clFiMWI2MW1mYzI5VlZpVWNFbWlMXC9QTWJsbCtvR2hWXC9ldmp5Nk8zZjRsZ0JTcTdUR1JJbFU4V29FYVd0RkVmbkpcL01qdis4NnVjQkJqXC96ZjFtZmFkb1FBd0FBIiwic3ViIjoiYWRtaW4iLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiZXhwIjoxNTIwNDI2NDM4LCJpYXQiOjE1MjA0MjI4Mzh9.D-Ky6RgRQZrbfd4I5W642cV2ymJXwlWSIUG-rVWBo3o";
 
 	public static void main(String[] args) {
-
+	
 		try {
 			DriverFactory.setFirefoxDriver();
 
@@ -44,6 +45,7 @@ public class Main {
 					StringEntity jsonBody = new StringEntity(page.getJson());
 					request.setEntity(jsonBody);
 					request.setHeader("Content-type", "application/json");
+					request.setHeader("X-Auth-Token", JWT);
 					HttpResponse response = httpClient.execute(request);
 					System.out.println(getJsonResponse(response));
 				} catch (Exception e) {
@@ -68,6 +70,7 @@ public class Main {
 		StringEntity jsonBody = new StringEntity(json.toString());
 		request.setEntity(jsonBody);
 		request.setHeader("Content-type", "application/json");
+		request.setHeader("X-Auth-Token", JWT);
 		HttpResponse response = httpClient.execute(request);
 		JsonObject responseObj = getJsonResponse(response);
 		url = responseObj.get("url").getAsString();
